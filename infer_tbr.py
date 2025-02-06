836f640a62d3fcb01cd7fa248b583f32d89a745b
Aditya P. Apte <aditya.apte@gmail.com>
2025-02-06 10:58:49 -0500

    Add files via upload
    Cleaned-up inference script, added phantom data

--- infer_tbr.py
+++ infer_tbr.py
@@ -41,17 +41,12 @@ def process_image(planC, strNum):
     identifier = {'imageType': 'PT SCAN'}
 
     grid_type = 'center'
-    #resamp_method = 'sitkBSpline'
     resamp_method = 'sitkLinear'
     mask_resamp_method = 'sitkNearestNeighbor'
     output_res = [0.195, 0.195, 0]  # Output res: 1mm x 1mm in-plane
 
-    resize_method = 'pad2d'
-    out_size = [32, 32]
-
     # Get scan array
-    #scan_num = getScanNumFromIdentifier(identifier, planC)[0]
-    scan_num = 1 # 2nd scan, which is zeroed out outside GTV
+    scan_num = 0
     x_vals, y_vals, z_vals = planC.scan[scan_num].getScanXYZVals()
     scan_arr = planC.scan[scan_num].getScanArray()
 
@@ -72,10 +67,6 @@ def process_image(planC, strNum):
                                      mask_resamp_method, inPlane=True) >= 0.5
     resamp_mask_arr = resamp_mask_arr.astype(int)
     resample_grid = [x_resample, y_resample, z_resample]
-    #planC = pc.importScanArray(resamp_scan_arr,
-    #                               resample_grid[0], resample_grid[1], resample_grid[2],
-    #                               modality, scan_num, planC)
-    #resample_scan_num = len(planC.scan) - 1
 
     return resamp_scan_arr, resamp_mask_arr, resample_grid
 
@@ -109,7 +100,6 @@ def main(argv):
               results_dir=output_path, serial_batches=False, suffix='', use_wandb=False, verbose=False,
               isTrain=False)
 
-    #opt = TestOptions().parse()  # get test options
     opt.num_threads = 0   # test code only supports num_threads = 0
     opt.batch_size = 1    # test code only supports batch_size = 1
     opt.serial_batches = True  # disable data shuffling; comment this line if results on randomly chosen images are needed.
@@ -139,13 +129,6 @@ def main(argv):
     planC = pc.loadNiiScan(ptScanFile, imageType="PT SCAN")
     planC = pc.loadNiiStructure(gtvSegFile, 0, planC, {1: 'GTV'})
 
-    # Zero out intensities outside the ROI
-    scan3M = planC.scan[0].getScanArray()
-    mask3M = rs.getStrMask(0, planC)
-    scan3M[~mask3M] = 0
-    xV, yV, zV = planC.scan[0].getScanXYZVals()
-    planC = pc.importScanArray(scan3M, xV, yV, zV, 'PT',0, planC)
-
     # Split GTV into components
     mask3M = rs.getStrMask(0, planC)
     labeledmask3M, numFeatures = label(mask3M, structure=np.ones((3, 3, 3)))
@@ -159,10 +142,9 @@ def main(argv):
     for strNum in range(1,numStructs):
 
         # Zero-out volume outside the GTV as done for training
-        #scan_vol, mask_vol, resize_grid, limits, resamp_mask_arr, resampGrid, valid_slices, planC = process_image(planC)
         scan_vol, mask_vol, resample_grid = process_image(planC, strNum)
         mask_vol = mask_vol.astype(float)
-        #scan_vol[mask_vol < 1] = 0
+        scan_vol[mask_vol < 1] = 0
         _, _, slcV = np.where(mask_vol)
         slcMin = slcV.min()
         slcMax = slcV.max()
@@ -176,6 +158,8 @@ def main(argv):
         for i in range(slcMin, slcMax+1):
             scan_slc = norm_scan[:,:,i]
             mask_slc = norm_mask[:,:,i]
+
+            # Crop to 32x32 matrix
             cropped_slc = - np.ones((32,32), dtype=float)
             cropped_msk = - np.ones((32,32), dtype=float)
             rowCols = np.where(mask_vol[:,:,i])
@@ -190,11 +174,7 @@ def main(argv):
             cropped_slc[rowStart:rowStart+deltaRow+1,colStart:colStart+deltaCol+1] = scan_slc[rMin:rMax+1,cMin:cMax+1]
             cropped_msk[rowStart:rowStart+deltaRow+1,colStart:colStart+deltaCol+1] = mask_slc[rMin:rMax+1,cMin:cMax+1]
 
-            #scan_slice = np.transpose(norm_scan[:,:,i].astype('<f4'))
-            #mask_slice = np.transpose(norm_mask[:,:,i].astype('<f4'))
-
-            #scan_slice = np.transpose(cropped_slc.astype('<f4'))
-            #mask_slice = np.transpose(cropped_msk.astype('<f4'))
+            # Create tensor
             scan_slice = cropped_slc.astype('<f4')
             mask_slice = cropped_msk.astype('<f4')
             zero_slice = -1 * np.ones(scan_slice.shape,dtype='<f4')
@@ -206,13 +186,12 @@ def main(argv):
             mask = mask[np.newaxis,np.newaxis,:,:]
             data = {'A': data_A, 'A_paths': '', 'mask': mask}
 
+            # Infer
             model.set_input(data)  # unpack data from data loader
             model.test()           # run inference
             visuals = model.get_current_visuals()  # get image results
-            real_A_tensor = visuals['real_A']
             fake_B_tensor = visuals['fake_B']
             tbr = fake_B_tensor.cpu().numpy()
-            #tbr_pred[rMin:rMax+1,cMin:cMax+1,i] = np.transpose(tbr[0,0,:cMax-cMin+1,:rMax-rMin+1])
             tbr_pred[rMin:rMax+1,cMin:cMax+1,i] = tbr[0,0,rowStart:rowStart+deltaRow+1,colStart:colStart+deltaCol+1]
 
         tbr_pred = TBR_max / 2 * (tbr_pred + 1)
@@ -227,6 +206,9 @@ def main(argv):
                                          resample_grid[0], resample_grid[1], resample_grid[2],
                                          xFDG, yFDG, zFDG,
                                          resampMethod, extrapVal, inPlane=False)
+
+        mask3M = rs.getStrMask(strNum, planC)
+        resampTBR[~mask3M] = 0
 
         tbrAllROIs3M += resampTBR
 
